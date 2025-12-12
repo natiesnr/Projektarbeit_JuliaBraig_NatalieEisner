37a519687c29c318197720ce176ec6946d83dddb
natiesnr <natalie.eisner@student.hnu.de>
2025-12-12 10:09:41 +0100

    alles gelöscht

--- src/main/java/studentenPlaner/Faecherauswahl.java
+++ src/main/java/studentenPlaner/Faecherauswahl.java
@@ -5,7 +5,4 @@ import javax.swing.*;
 public class Faecherauswahl {
 
     private JPanel hauptPanel;
-    private JLabel halloLabel;
-    private JTextField hallotextField1;
-    private JButton hilfeButton;
 }
