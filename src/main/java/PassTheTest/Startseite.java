b1c74fd71e5d190ebfed8cfbd1c0f585940ee1b7
jubr7055 <julia.braig@student.hnu.de>
2025-12-12 12:59:56 +0100

    1 Versuch SS

--- src/main/java/PassTheTest/Startseite.java
+++ src/main/java/PassTheTest/Startseite.java
@@ -1,4 +1,23 @@
 package PassTheTest;
 
-public class Startseite {
+import javax.swing.*;
+
+public class Startseite extends JFrame {
+    private JLabel ueberschriftLabel;
+    private JPanel hauptPanel;
+    private JButton faecherbutton1;
+    private JButton organizerbutton1;
+    private JButton notenbutton1;
+    private JTextField modusTextField;
+
+    public Startseite() {
+        setTitle("Startseite");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(640, 700);
+        setContentPane(hauptPanel);
+        setVisible(true);
+
+    }
+
 }
+
